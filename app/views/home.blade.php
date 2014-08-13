d6dfb05d1068477bbeaa0ae9a9c6543e54b57b06
Finleyrc4 <finleyrc4@gmail.com>
2014-08-13 12:44:23 -0400

    Home text

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
-	This is some content. Whooo it works!
+	This is the website of the Official League of Legends Club at the University of Cincinnati.
+	We were founded in the Spring of 2013 and are part of Riot Games’ Official Collegiate Program. 
+	Our purpose is to grow a sense of community centered around League of Legends as well as to organize 
+	events that cater to both the casual and competitive player.
+
 @stop
