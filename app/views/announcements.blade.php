011d637c25e73eb4a5a3388371a44326d25a454b
Finleyrc4 <finleyrc4@gmail.com>
2014-08-13 12:47:06 -0400

    Annoucements text not formatted

--- app/views/announcements.blade.php
+++ app/views/announcements.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.master')
 
 @section('content')
-	This is the announcements page.
+	WE HAVE A NEW WEBSITE! 
+The group known as UCLoL has existed for almost 1 year and 9 months now, and in that period of time we have seen explosive growth - undeniably one of the fastest growing clubs at the University of Cincinnati.  From starting out as a 25 person Facebook group, we are now an organization fast approaching 500+ members.  
+
+As we continued to grow, we realized that we needed to update our website to be more modern and customized to fit our new needs. 
+WHAT KIND OF FEATURES WILL THIS WEBSITE HAVE?
+This website has a boat-load of features that we have currently installed on it, and we will always be looking at ways to constantly improve it.  
+
+Currently these are the list of the features that you can expect to find on our website:
+
+Calendar:
+We are going to have a link to our official event calendar on google docs. On this link you can view all upcoming and past events such as tournaments, meetings, social events, planned club in-houses and more!
+
+Our Community+ : 
+As most of you know, we became an official club sport last year. We host tryouts for our 5v5 ranked team in September. Last year we participated in the IvyLoL League and plan to again this year. If you are interested in trying out for this year’s team, click on the “Tryouts” link under “Our Community+”.
+We have upgraded our members list from a Facebook Document, to a Google Document. On this document, you can view other UCLoL members’ Summoner Names, rank, preferred position, and favorite champions. This should be used to quickly find other players around your skill level to duo with or make ranked teams.  
+As a group, we have two dedicated Stream Channels which we use to show and record our events!  We can also host the Stream Information of our members, who might wish to make themselves known to our community, so that you can view your friends and mentors directly from our website in an easy to use fashion!
+The link to our own TeamSpeak 3 server is located in this section too. We use TeamSpeak 3 because it takes up less bandwidth while playing (cause everyone hates lag) and because it is also free for our entire club to use!
+
+Sincerely,
+Chris Postell & the rest of the Staff
+
 @stop
