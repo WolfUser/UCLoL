4f75f25ba4f0d4f0217607a326746c1e7ba4aa60
Finleyrc4 <finleyrc4@gmail.com>
2014-08-14 15:24:21 -0400

    Bug fix on Community Tab and Calendar Link

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.master')
 
 @section('content')
-	The link to the Google Doc with our calendar of events will be added later.
+	<p>Below you will find the link to the google doc calendar that will be using to plan all of our events. Feel
+	free to check it out if you wonder when a certain event is.</br>
+	</br>
+	Click <a href="#">Here</a> for the UCLoL Event Calendar.</p>
 @stop
