a35201fb8c9a6cc82acdf2ec45fb08232c30ddcc
Finleyrc4 <finleyrc4@gmail.com>
2014-08-14 09:42:28 -0400

    Text Changes to Our Staff

--- app/views/ourstaff.blade.php
+++ app/views/ourstaff.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-	President - Chris Postell
-	Vice President - Juanita Dickhaus
-	Treasurer - Richard Baxter
-	Event Planner - Terrance Webb
-	Media and Website Organizer - Ryan Finley
-	Ranked Teams Coordinator - Jon Deibel
-	Assistance Social Event Planner - Jon Wood
+	<p>
+	President - Chris Postell</br>
+	Vice President - Juanita Dickhaus </br>
+	Treasurer - Richard Baxter</br>
+	Event Planner - Terrance Webb</br>
+	Media and Website Organizer - Ryan Finley</br>
+	Ranked Teams Coordinator - Jon Deibel</br>
+	Assistance Social Event Planner - Jon Wood</br>
+	</p>
 @stop
