0018d2770cd277ec9dc2ad57a6075b5fb2da26fc
Finleyrc4 <finleyrc4@gmail.com>
2014-08-14 15:26:21 -0400

    More Bold stuff

--- app/views/community/teamspeak.blade.php
+++ app/views/community/teamspeak.blade.php
@@ -4,7 +4,7 @@
 	<p>UCLoL uses TeamSpeak 3 for its memebers to use while playing League and other games together.
 	The link to download teamspeak and the server info are below: </br>
 	<b>Download link:</b> <a href="http://www.teamspeak.com/?page=downloads">Teamspeak 3</a></br>
-	Server Address (used to bookmark our server for easy access): uclol.teamspeak3.com
+	<b>Server Address (used to bookmark our server for easy access):</b> uclol.teamspeak3.com
 	</p>
 
 @stop
