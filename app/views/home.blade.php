61fe0e8d14c4a988c06bea31d8e25d0569a12eda
Finleyrc4 <finleyrc4@gmail.com>
2014-08-14 10:03:34 -0400

    Centered Picture

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,6 +5,6 @@
 	We were founded in the Spring of 2013 and are part of Riot Games’ Official Collegiate Program. 
 	Our purpose is to grow a sense of community centered around League of Legends as well as to organize 
 	events that cater to both the casual and competitive player. </p>
-	<img border="0" src="img/LargeLogo.png" alt="UCLoL Logo" width="630" height="648">
+	<center><img border="0" src="img/LargeLogo.png" alt="UCLoL Logo" width="630" height="648"></center>
 
 @stop
