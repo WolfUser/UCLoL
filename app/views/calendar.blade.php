144c9a10933a6c300a3869a1811e303ec65cfd26
Finleyrc4 <finleyrc4@gmail.com>
2014-08-14 20:23:24 -0400

    Link added

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -4,5 +4,5 @@
 	<p>Below you will find the link to the google doc calendar that will be using to plan all of our events. Feel
 	free to check it out if you wonder when a certain event is.</br>
 	</br>
-	Click <a href="#">Here</a> for the UCLoL Event Calendar.</p>
+	Click <a href="https://www.google.com/calendar/embed?src=uofcincinnatilol%40gmail.com&ctz=America%2FNew_York">Here</a> for the UCLoL Event Calendar.</p>
 @stop
