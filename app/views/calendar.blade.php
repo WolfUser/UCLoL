6b9e9deecab7153122e81e3ab5be311b196dfd87
Dibes <dibesjr@gmail.com>
2014-08-21 01:03:42 -0400

    Calendar Test

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-	<p>Below you will find the link to the google doc calendar that will be using to plan all of our events. Feel
-	free to check it out if you wonder when a certain event is.</br>
-	</br>
-	Click <a href="https://www.google.com/calendar/embed?src=uofcincinnatilol%40gmail.com&ctz=America%2FNew_York">Here</a> for the UCLoL Event Calendar.</p>
+	<div class="block block-text_block span12 first cf">
+		<div class="title-wrapper">
+			<h3 class="widget-title">Calendar</h3>
+			<div class="clear"></div>
+		</div>
+		<div class="wcontainer">
+			<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=uofcincinnatilol%40gmail.com&amp;color=%230F4B38&amp;ctz=America%2FNew_York" style=" border-width:0 " width="100%" height="800" frameborder="0" scrolling="no"></iframe>
+		</div>
+	</div>
 @stop
