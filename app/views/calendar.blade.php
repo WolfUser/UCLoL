2378e5e995b045f9637f58df48e92a9e609785a4
Dibes <dibesjr@gmail.com>
2014-08-21 01:06:57 -0400

    Calendar push

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="block block-text_block span12 first cf">
-		<div class="title-wrapper">
+		<div class="title-wrapper" style="margin-left:3px;">
 			<h3 class="widget-title">Calendar</h3>
 			<div class="clear"></div>
 		</div>
 		<div class="wcontainer">
 			<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=uofcincinnatilol%40gmail.com&amp;color=%230F4B38&amp;ctz=America%2FNew_York" style=" border-width:0 " width="100%" height="800" frameborder="0" scrolling="no"></iframe>
 		</div>
-	</div>
 @stop
