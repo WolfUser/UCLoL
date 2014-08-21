98b7b5343e1c3103a99909c6cd62a248a9af8b76
Dibes <dibesjr@gmail.com>
2014-08-21 01:25:54 -0400

    Link Fixed

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -6,5 +6,14 @@
 			<div class="clear"></div>
 		</div>
 		<div class="wcontainer">
-			<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=uofcincinnatilol%40gmail.com&amp;color=%230F4B38&amp;ctz=America%2FNew_York" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>		</div>
+			<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=uofcincinnatilol%40gmail.com&amp;color=%230F4B38&amp;ctz=America%2FNew_York" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+		</div>
+
+		<style type="text/css">
+
+			.view-cap, .view-container-border {
+				background-color:transparent;
+			}
+
+		</style>
 @stop
