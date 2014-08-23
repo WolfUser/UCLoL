5190221c45d0db2e6d14f2338035234b907534c7
Ryan Finley <finleyrc4@gmail.com>
2014-08-23 13:21:35 -0400

    Tryouts Page Update

--- app/views/community/tryouts.blade.php
+++ app/views/community/tryouts.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.master')
 
 @section('content')
-	The tryouts information will be posted closer to September when the tryouts will be held.
+	<p>
+	August 22nd: People can start submitting their soloq info. There is a minimum rank requirement of Gold V to be able to try out. </br>
+
+	September 1st - 5th: Soloq reviewing officially starts and details will be logged <a href="http://goo.gl/cjltMB">Here</a></br>
+
+	September 8th-12th: Teams will scrim against eachother. Ideally each team will play 3 times and preferably every person will play 3 different 
+	champs in their designated role. </br>
+
+	September 13th: Main teams get tentatively decided.
+	</p>
 @stop
