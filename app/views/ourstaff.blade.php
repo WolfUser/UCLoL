caf00170ad963ed43130bd0e73a6d3598dbce845
Ryan Finley <finleyrc4@gmail.com>
2014-08-26 19:32:51 -0400

    Changes to ourstaff

--- app/views/ourstaff.blade.php
+++ app/views/ourstaff.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 	<p>
-	<b>President -</b> Chris Postell</br>
+	<b>President -</b> Chris Postell </br> <img border="0" src="/images/Chris.jpg" alt="Chris" width="304" height="228">
 	<b>Vice President -</b> Juanita Dickhaus </br>
-	<b>Treasurer -</b> Richard Baxter</br>
+	<b>Treasurer -</b> Richard Baxter </br>
 	<b>Event Planner -</b> Terrance Webb</br>
-	<b>Media and Website Organizer -</b> Ryan Finley</br>
-	<b>Ranked Teams Coordinator -</b> Jon Deibel</br>
-	<b>Assistant Social Event Planner -</b> Jon Wood</br>
+	<b>Media and Website Organizer -</b> Ryan Finley </br>
+	<b>Ranked Teams Coordinator -</b> Jon Deibel </br>
+	<b>Assistant Social Event Planner -</b> Jon Wood </br>
 	</p>
 @stop
