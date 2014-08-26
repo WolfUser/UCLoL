8085411389657b9420bc92138dc0945c9b76a0c2
Ryan Finley <finleyrc4@gmail.com>
2014-08-26 19:34:11 -0400

    typeo

--- app/views/ourstaff.blade.php
+++ app/views/ourstaff.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<p>
-	<b>President -</b> Chris Postell </br> <img border="0" src="/images/Chris.jpg" alt="Chris" width="304" height="228">
+	<b>President -</b> Chris Postell </br> <img border="0" src="img/Chris.jpg" alt="Chris" width="304" height="228">
 	<b>Vice President -</b> Juanita Dickhaus </br>
 	<b>Treasurer -</b> Richard Baxter </br>
 	<b>Event Planner -</b> Terrance Webb</br>
