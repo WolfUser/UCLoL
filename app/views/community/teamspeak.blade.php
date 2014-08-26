ce58a901a53b40b073f7d956edcf75bdcd2fe900
Dibes <dibesjr@gmail.com>
2014-08-26 19:30:33 -0400

    Added teamspeak info

--- app/views/community/teamspeak.blade.php
+++ app/views/community/teamspeak.blade.php
@@ -4,7 +4,9 @@
 	<p>UCLoL uses TeamSpeak 3 for its memebers to use while playing League and other games together.
 	The link to download teamspeak and the server info are below: </br>
 	<b>Download link:</b> <a href="http://www.teamspeak.com/?page=downloads">Teamspeak 3</a></br>
-	<b>Server Address (used to bookmark our server for easy access):</b> uclol.teamspeak3.com
-	</p>
+	<b>Server Address (used to bookmark our server for easy access):</b> uclol.teamspeak3.com <br>
+	<div style="overflow-y:scroll;height:300px;">
+		<script type="text/javascript" src="http://view.light-speed.com/teamspeak3.php?IP=uclol.teamspeak3.com&PORT=6202&QUERY=10013&UID=237250&display=none&font=11px&background=transparent&server_info_background=transparent&server_info_text=%23222&server_name_background=transparent&server_name_text=%23222&info_background=transparent&channel_background=transparent&channel_text=%23222&username_background=transparent&username_text=%23222"></script>
+	</div>
 
 @stop
