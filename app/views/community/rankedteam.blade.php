936dbcec714938c8e76333bf3048b0e23628a950
Dibes <dibesjr@gmail.com>
2014-09-23 10:37:29 -0400

    Update rankedteam.blade.php

--- app/views/community/rankedteam.blade.php
+++ app/views/community/rankedteam.blade.php
@@ -7,8 +7,10 @@
 	<div class="title-wrapper">
 	    <h3 class="widget-title">UC TEAM BLACK</h3>
 	    <div class="clear"></div>
-	    <div id="template-wrapper-88" class="template-wrapper" style="margin-top:20px;">
-	    <div id="block-1" class="block block-column_block span12 cf"><div id="block-2" class="block block-team_member_block span4 first cf"><div class="member"><div class="member-social"><a data-original-title="Facebook" data-toggle="tooltip" href="#" class="icon-facebook-sign"></a><a data-original-title="Twitter" data-toggle="tooltip" href="#" class="icon-twitter-sign"></a> <a data-original-title="Youtube" data-toggle="tooltip" href="#" class="icon-youtube"></a></div>
+	    <div id="template-wrapper-88" class="template-wrapper">
+	    <div id="block-1" class="block block-column_block span12 cf">
+	    
+	    <div id="block-2" class="block block-team_member_block span4 cf"><div class="member"><div class="member-social"></div>
 			
                 <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team1.jpg" alt="Clan Members">
                 <h3 class="widget-title">Bryan Hoskins (Bryanman3320)</h3>
