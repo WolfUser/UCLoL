46bcc8c10c3256f3e008e699df38f74c55de0cf6
Dibes <dibesjr@gmail.com>
2014-09-23 10:34:54 -0400

    Update rankedteam.blade.php

--- app/views/community/rankedteam.blade.php
+++ app/views/community/rankedteam.blade.php
@@ -5,10 +5,10 @@
 <div class="row block">
 	<div class="block block-contactform_block span12">
 	<div class="title-wrapper">
-	    <h3 class="widget-title">COMPETITIVE TEAM</h3>
+	    <h3 class="widget-title">UC TEAM BLACK</h3>
 	    <div class="clear"></div>
-	    <div id="template-wrapper-88" class="template-wrapper">
-	    <div id="block-1" class="block block-column_block span12 first cf"><div id="block-2" class="block block-team_member_block span4 first cf"><div class="member"><div class="member-social"><a data-original-title="Facebook" data-toggle="tooltip" href="#" class="icon-facebook-sign"></a><a data-original-title="Twitter" data-toggle="tooltip" href="#" class="icon-twitter-sign"></a> <a data-original-title="Youtube" data-toggle="tooltip" href="#" class="icon-youtube"></a></div>
+	    <div id="template-wrapper-88" class="template-wrapper" style="margin-top:20px;">
+	    <div id="block-1" class="block block-column_block span12 cf"><div id="block-2" class="block block-team_member_block span4 first cf"><div class="member"><div class="member-social"><a data-original-title="Facebook" data-toggle="tooltip" href="#" class="icon-facebook-sign"></a><a data-original-title="Twitter" data-toggle="tooltip" href="#" class="icon-twitter-sign"></a> <a data-original-title="Youtube" data-toggle="tooltip" href="#" class="icon-youtube"></a></div>
 			
                 <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team1.jpg" alt="Clan Members">
                 <h3 class="widget-title">Bryan Hoskins (Bryanman3320)</h3>
@@ -22,7 +22,7 @@
                 <div class="content"><p>The dedicated jungler for UC Team Black!</p>
 </div>
 				<div class="blacksq"></div>
-             </div></div><div id="block-4" class="block block-team_member_block span4 first cf"><div class="member"><div class="member-social"></div>
+             </div></div><div id="block-4" class="block block-team_member_block span4 cf"><div class="member"><div class="member-social"></div>
 			
                 <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team21.jpg" alt="Clan Members">
                 <h3 class="widget-title">Nicholas Gramke (IncendiarySpade)</h3>
@@ -37,7 +37,15 @@
 </div>
 				<div class="blacksq"></div>
 			
-             </div></div></div></div>
+             </div></div>
+             
+             <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team3.jpg" alt="Clan Members">
+                <h3 class="widget-title">Jon MacBeth (JonMacTubbs)</h3>
+                <div class="content"><p>The dedicated support for UC Team Black!</p>
+</div>
+				<div class="blacksq"></div>
+             
+             </div></div>
 	
 	</div>
 	</div>
