f3a5a7de9ba2e9a2ca38940cec9eef45c33d2929
Dibes <dibesjr@gmail.com>
2014-09-23 10:29:22 -0400

    Update rankedteam.blade.php

--- app/views/community/rankedteam.blade.php
+++ app/views/community/rankedteam.blade.php
@@ -7,9 +7,42 @@
 	<div class="title-wrapper">
 	    <h3 class="widget-title">COMPETITIVE TEAM</h3>
 	    <div class="clear"></div>
-	</div>
-	<div class="mcontainer">
-	    This will be filled out once the tryouts have been finished
+	    <div id="block-1" class="block block-column_block span8 first cf"><div id="block-2" class="block block-team_member_block span4 first cf"><div class="member"><div class="member-social"><a data-original-title="Facebook" data-toggle="tooltip" href="#" class="icon-facebook-sign"></a><a data-original-title="Twitter" data-toggle="tooltip" href="#" class="icon-twitter-sign"></a> <a data-original-title="Youtube" data-toggle="tooltip" href="#" class="icon-youtube"></a></div>
+			
+                <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team1.jpg" alt="Clan Members">
+                <h3 class="widget-title">Bryan Hoskins (Bryanman3320)</h3>
+                <div class="content"><p>The dedicated top lane for UC Team Black!</p>
+</div>
+				<div class="blacksq"></div>
+             </div></div><div id="block-3" class="block block-team_member_block span4  cf"><div class="member"><div class="member-social"></div>
+			
+                <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team3.jpg" alt="Clan Members">
+                <h3 class="widget-title">Trevor Rusch (therusch)</h3>
+                <div class="content"><p>The dedicated jungler for UC Team Black!</p>
+</div>
+				<div class="blacksq"></div>
+             </div></div><div id="block-4" class="block block-team_member_block span4 first cf"><div class="member"><div class="member-social"></div>
+			
+                <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team21.jpg" alt="Clan Members">
+                <h3 class="widget-title">Nicholas Gramke (IncendiarySpade)</h3>
+                <div class="content"><p>The returning mid laner for UC Team Black!</p>
+</div>
+				<div class="blacksq"></div>
+             </div></div><div id="block-5" class="block block-team_member_block span4  cf"><div class="member"><div class="member-social"></div>
+			
+                <img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team4.jpg" alt="Clan Members">
+                <h3 class="widget-title">ShaoChu Liu (iAce)</h3>
+                <div class="content"><p>The dedicated ADC for UC Team Black!</p>
+</div>
+				<div class="blacksq"></div>
+				
+		<img src="http://skywarriorthemes.com/gameaddict/wp-content/uploads/2014/01/team4.jpg" alt="Clan Members">
+                <h3 class="widget-title">Jon MacBeth (JonMacTubbs)</h3>
+                <div class="content"><p>The dedicated support for UC Team Black!</p>
+</div>
+				<div class="blacksq"></div>
+             </div></div></div>
+	
 	</div>
 	</div>
 </div>
