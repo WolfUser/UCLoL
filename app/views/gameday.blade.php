c751d21ce0bfbdf3342306ec01f33d5f1eae43aa
Ryan Finley <finleyrc4@gmail.com>
2014-09-27 16:14:40 -0400

    League info

--- app/views/gameday.blade.php
+++ app/views/gameday.blade.php
@@ -2,13 +2,36 @@
 
 @section('content')
 <p>
-	Game day will have 5 different events happening throughout the day. They are: </br>
+	Game day will have 5 different clubs hosting events throughout the day. 
+	</br> They are: 
 	- League of Legends </br>
 	- Magic the Gathering </br>
 	- Anime </br>
 	- Hearthstone </br>
 	- Console Gaming </br>
 	</br>
+	UCLoL is providing food, consoles, games, tournaments, raffles, and giveaways! UC:MTG will be hosting tables for 
+	(you guessed it) MTG! UC Hearthstone will be hosting UC's first in-person Hearthstone tournament, and Anime UC 
+	will be showing anime, hosting some console games, and providing awesome Japanese food, candy, and drinks!
+	</br>
+	For more information on a certain club's events, see below. </br>
+	--------------------------------------------------------------------------- </br>
+	</br>
+	<b> League of Legends Information </b></br>
+	<u>** Tournament Registration (OPEN or sign up @ event) ** </u> </br>
+	LoL (5v5): http://battlefy.com/uclol/uclol-5v5-fall-tournament/5407e3ca3878a9d1089ffab1/info </br>
+	LoL (1v1): Sign up @ event </br>
+	</br>
+	** Ruleset for League ** </br>
+	https://docs.google.com/document/d/1qPF7Ym1tdgYSnzvmY-90wxhObRv_a_wjoNMxhFWA94s/edit?usp=sharing (feel free to contact Juanita Dickhaus) </br>
+	</br>
+	** PRIZING ** </br>
+	LoL (5v5):  </br>
+	1st place: 3200 RP and Triumphant Ryze skin for every player (more prizes TBA) </br>
+	2nd place: 2400 RP per player </br>
+	3rd place: 1600 RP per player </br>
+	4th place: 800 RP per player </br>
+	LoL (1v1): A selection from our SWAG BAG (and one more prize TBA) </br>
 
 </p>
 
