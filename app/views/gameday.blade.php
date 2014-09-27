763f4f5df37193b31cc02667b8bddb6591031b5f
Ryan Finley <finleyrc4@gmail.com>
2014-09-27 16:04:08 -0400

    First Paragraph

--- app/views/gameday.blade.php
+++ app/views/gameday.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
 <p>
-	<b> GAME DAY TEST TEXT </b>
+	Game day will have 5 different events happening throughout the day. They are: </br>
+	- League of Legends </br>
+	- Magic the Gathering </br>
+	- Anime </br>
+	- Hearthstone </br>
+	- Console Gaming </br>
+	</br>
+
 </p>
 
 @stop
