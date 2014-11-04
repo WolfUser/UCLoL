b3b5371f97e1efdd25f5056b4bdd41003addf983
Dibes <dibesjr@gmail.com>
2014-11-04 13:09:15 -0500

    Re-did the ranked team page

--- app/views/community/rankedteam.blade.php
+++ app/views/community/rankedteam.blade.php
@@ -12,8 +12,8 @@
 	    
 	    <div id="block-2" class="block block-team_member_block span4 cf"><div class="member"><div class="member-social"></div>
 			
-                <img src="http://24.media.tumblr.com/d8dc5813eafce7847f234aac9d8a2d5b/tumblr_mj9gjxj3RA1rczihjo1_500.jpg" alt="Clan Members">
-                <h3 class="widget-title">Bryan Hoskins (Bryanman3320)</h3>
+                <img src="http://i.imgur.com/jN8XBbg.jpg" alt="Clan Members">
+                <h3 class="widget-title">Rafael Heck (Gh0stbr)</h3>
                 <div class="content"><p>The dedicated top lane for UC Team Black!</p>
 </div>
 				<div class="blacksq"></div>
@@ -34,7 +34,7 @@
              </div></div><div id="block-5" class="block block-team_member_block span4  cf"><div class="member"><div class="member-social"></div>
 			
                 <img src="http://31.media.tumblr.com/tumblr_mry3zmarwy1rle77go1_1377196979_cover.jpg" alt="Clan Members">
-                <h3 class="widget-title">ShaoChu Liu (iAce)</h3>
+                <h3 class="widget-title">Ryan Finley (Finley)</h3>
                 <div class="content"><p>The dedicated ADC for UC Team Black!</p>
 </div>
 				<div class="blacksq"></div>
@@ -43,8 +43,8 @@
              
              <div id="block-6" class="block block-team_member_block span4 cf"><div class="member"><div class="member-social"></div>
 			
-                <img src="https://i1.sndcdn.com/artworks-000062773952-fgkjog-t500x500.jpg?e76cf77" alt="Clan Members">
-                <h3 class="widget-title">Jon MacBeth (JonMacTubbs)</h3>
+                <img src="http://fc06.deviantart.net/fs71/f/2013/195/e/9/thresh__the_chain_warden_by_dismalfiction-d6dh6k0.jpg" alt="Clan Members">
+                <h3 class="widget-title">Bryan Hoskins (Bryanman3320)</h3>
                 <div class="content"><p>The dedicated support for UC Team Black!</p>
 </div>
 				<div class="blacksq"></div>
