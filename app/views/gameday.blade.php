b9506d919e840e45cdafcef5441c0f430453d067
Ryan Finley <finleyrc4@gmail.com>
2015-01-08 11:03:25 -0500

    LoL Game Day Info
    changed game day info for LoL. First Try

--- app/views/gameday.blade.php
+++ app/views/gameday.blade.php
@@ -2,53 +2,50 @@
 
 @section('content')
 <p>
-	Game day will have 5 different clubs hosting events throughout the day. </br>
-	They are: </br>
+	<b> Game Day will be held on February 7th in the Swift Building. </b> </br>
+	Game Day will have many different events throughout the day. Please see below to find the game you wish to know the gaming schedule of. </br>
+	Games with major events are are: </br>
 	- League of Legends </br>
-	- Magic the Gathering </br>
-	- Anime </br>
+	- DotA2</br>
 	- Hearthstone </br>
-	- Console Gaming </br>
 	</br>
-	UCLoL is providing food, consoles, games, tournaments, raffles, and giveaways! UC:MTG will be hosting tables for 
-	(you guessed it) MTG! UC Hearthstone will be hosting UC's first in-person Hearthstone tournament, and Anime UC 
-	will be showing anime, hosting some console games, and providing awesome Japanese food, candy, and drinks! </br>
-	As you enter, you will be given one raffle ticket that will used for a chance to win a Plantronics Gaming Headset
-	and other awesome prizes.
 	</br> </br>
 	<b> ** General Schedule for non-game events** </b> </br>
-	12:00 PM - Doors Open </br>
-	3:00 PM - Pizza arrives  </br>
-	6:00 PM - Raffle Giveaways and cookies </br>
+	12:00 PM - Doors Open for gamers </br>
+	2:30 PM - Food arrives for those not in tournament games  </br>
+	3:15 PM - Food arrives for those in tournament games </br>
+	5:30 PM - Food and dessert arrives for everyone
 	8:00 PM - Clean up begins </br>
 	</br>
-	For more information on a certain club's events, see below. </br></br>
+	</br>
+	For more information on a certain games's events, see below. </br></br>
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
-	<b> League of Legends Information - Room 800 </b></br>
+	<b> League of Legends Information - Rooms 817, 701, and 702 </b></br>
 	<b>** Tournament Registration ** </b> </br>
-	LoL (5v5): <a href=http://battlefy.com/uclol/uclol-5v5-fall-tournament/5407e3ca3878a9d1089ffab1/info> Sign up here with the password UC </a> </br>
+	LoL (5v5): <a href=http://battlefy.com/postelcj/uc-game-day-feb-7th/54ae8e1024c0fc9d00542358/info> Sign up here with the password UC </a> </br>
 	LoL (1v1): Sign up at event </br>
 	</br>
 	<b>** Ruleset for League **</b> </br>
-	<a href=https://docs.google.com/document/d/1qPF7Ym1tdgYSnzvmY-90wxhObRv_a_wjoNMxhFWA94s/edit?usp=sharing>Link to Ruleset</a> 
-	(feel free to contact Juanita Dickhaus) </br>
+	<a href=https://docs.google.com/document/d/1qPF7Ym1tdgYSnzvmY-90wxhObRv_a_wjoNMxhFWA94s/edit?usp=sharing>Link to Ruleset</a> </br>
 	</br>
 	<b>** PRIZING **</b> </br>
 	<b>LoL (5v5):</b>  </br>
-	1st place: 3200 RP, Triumphant Ryze skin for every player, and 5 Plantronics Gaming Headsets! </br>
+	1st place: 3200 RP, Triumphant Ryze skin for every player, and more special prizes! </br>
 	2nd place: 2400 RP per player </br>
 	3rd place: 1600 RP per player </br>
 	4th place: 800 RP per player </br>
 	<b>LoL (1v1): </b> </br> 
-	A selection from our SWAG BAG </br></br>
-	<b>  ***** ANYONE who signs up for a 1v1 or 5v5 tournament will be eligble to win a Plantronics Gaming Headset! *****</b> </br></br>
+	A selection from our SWAG BAG </br>
+	LoL 1v1 tourney will begin around 4:30 PM. Sign ups are in person at the event and will be held in Room 800. </br>
+	</br>
 
-	<b>** Schedule ** </b> </br>
-	12:30 PM - First 1v1 tourney begins </br>
-	1:00 PM - 5v5 registration closes and matches begin </br>
-	3:00 PM - 2nd 1v1 tourney begins </br>
-	5:00 PM - Finals for 5v5 begin (tenatively) </br>
-	6:00 PM - Inhouses, chase the teemo, etc. </br></br>
+	<b>** Schedule for 5v5 Tournament** </b> </br>
+	12:00 PM - 5v5 registration opens </br>
+	1:00 PM - 5v5 registration closes and Match 1 begins </br>
+	2:30 PM - Match 2 of 5v5 tournament begins </br>
+	4:00 PM - Match 3 of 5v5 tournament begins </br>
+	6:00 PM - Best of 3 Championship match for tournament title </br>
+	</br>
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
 	<b> Console Gaming Information - Room 819 </b></br>
 	<b>** Tournament Registration (sign up at event) ** </b> </br>
