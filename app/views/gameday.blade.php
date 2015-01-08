2c1b68d165fc99f64ec661f02409586ffcfdc256
Ryan Finley <finleyrc4@gmail.com>
2015-01-08 11:18:49 -0500

    Most of game day changes made
    ^

--- app/views/gameday.blade.php
+++ app/views/gameday.blade.php
@@ -14,15 +14,15 @@
 	12:00 PM - Doors Open for gamers </br>
 	2:30 PM - Food arrives for those not in tournament games  </br>
 	3:15 PM - Food arrives for those in tournament games </br>
-	5:30 PM - Food and dessert arrives for everyone
+	5:30 PM - Food and dessert arrives for everyone </br>
 	8:00 PM - Clean up begins </br>
 	</br>
 	</br>
-	For more information on a certain games's events, see below. </br></br>
+	For more information on a certain game's events, see below. </br></br>
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
 	<b> League of Legends Information - Rooms 817, 701, and 702 </b></br>
 	<b>** Tournament Registration ** </b> </br>
-	LoL (5v5): <a href=http://battlefy.com/postelcj/uc-game-day-feb-7th/54ae8e1024c0fc9d00542358/info> Sign up here with the password UC </a> </br>
+	LoL (5v5): <a href=http://battlefy.com/postelcj/uc-game-day-feb-7th/54ae8e1024c0fc9d00542358/info> Sign up here</a> </br>
 	LoL (1v1): Sign up at event </br>
 	</br>
 	<b>** Ruleset for League **</b> </br>
@@ -40,6 +40,7 @@
 	</br>
 
 	<b>** Schedule for 5v5 Tournament** </b> </br>
+	(Note: Game times may vary depending on number of teams that sign up and duration of matches)
 	12:00 PM - 5v5 registration opens </br>
 	1:00 PM - 5v5 registration closes and Match 1 begins </br>
 	2:30 PM - Match 2 of 5v5 tournament begins </br>
@@ -47,15 +48,15 @@
 	6:00 PM - Best of 3 Championship match for tournament title </br>
 	</br>
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
-	<b> Console Gaming Information - Room 819 </b></br>
+	<b> Console Gaming Information - Room 816 </b></br>
 	<b>** Tournament Registration (sign up at event) ** </b> </br>
-	Super Smash Bros Melee / Project M singles / doubles: TBD (contact Juanita with questions/comments) </br></br>
 	<b> Games that will be played: </b> </br>
 	- BlazBlue: Continuum Shift (360) </br>
 	- Super Street Fighter 4 (360) </br>
 	- Mario Kart 8 (Wii U) </br>
 	- Hyrule Warriors (Wii U) </br>
 	- Super Smash Bros. Brawl (Wii U) </br></br>
+	- And even more... </br>
 
 	<b>** Ruleset for Console Gaming **</b> </br>
 	Smash Bros: <a href=http://smashboards.com/rulesets/melee/>Link to Ruleset</a> </br>
@@ -71,32 +72,40 @@
 	12:00 PM - Console Tournament sign ups begin </br>
 	12:30 PM - Warm ups begin for games </br>
 	1:30 PM - Sign ups end for tournaments </br>
-	2:00 PM - Console Tournaments begin </br>
+	2:00 PM - Console Tournaments begin and continue until around 5:00 PM </br>
 	2:30 PM - Systems not being used for tournaments are open to free play. </br></br>
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
-	<b> Magic the Gathering Information - Room 816 </b></br>
+	<b> DotA2 Information </b></br>
+	<b>** Tournament Registration: sign up at event ** </b> </br>
 	</br>
-	Make sure to bring your own deck if you wish to play. </br>
-	This will likely be a challenge based area where people just play eachother for fun and games can be watched.</br></br>
+	<b>** Schedule ** </b> </br>
+	(Note: Game times may vary depending on number of teams that sign up and duration of matches)
+	12:00 PM - Registration opens </br>	 
+	1:00 PM - 5v5 registration closes and Match 1 begins </br>
+	2:15 PM - Match 2 of 5v5 tournament begins </br>
+	3:15 PM - Match 3 of 5v5 tournament begins </br>
+	5:00 PM - Best of 5 Championship match for tournament title </br></br>
+
+	
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
 	<b> Hearthstone Information - Room 819 </b></br>
 	<b>** Tournament Registration: sign up at event ** </b> </br>
 	</br>
-	<b>** Ruleset for League **</b> </br>
-	Contact Kase or Brad or see the UC Hearthstone <a href= https://www.facebook.com/groups/255773791258321/>Facebook Page</a>. </br>
+	<b>** Ruleset **</b> </br>
+	The tournament will be played in a "Duel" format. This format has each played have only one deck for one class that they use for the entire tournament. Before
+	the player faces their opponent, they may use a sideboard of 10 cards to alter their deck.
+	For more information on the "Duel" format, see  <a href=http://www.tryingtobegosu.com/2014/12/review-worlds-1st-hearthstone-tournament-to-use-duel/> here</a>  </br>
 	</br>
-	<b>** PRIZING **</b> </br>
-	1st Place: Plantronics Gaming Headset and more (TBA) </br></br>
 	<b>** Schedule ** </b> </br>
-	12:00 PM - Registration opens for 1v1 round robin, free play </br>	 
-	1:00 PM - Registration closes and 1v1 starts </br>		
-	4:00 PM - Final 4 double elim finals </br>		 
-	6:00 PM - Auto Deck tournament (no prizes)	</br></br>	
+	1:00 PM - Registration opens
+	2:00 PM - Registration closes and tournament 1 begins
+	4:00 PM - Tournament over
+	5:00 PM - Registration opens for next tournament
+	6:00 PM - Registration closes and tournament 2 begins
+	8:00 PM - Tournament over
 	
 	------------------------------------------------------------------------------------------------------------------------------------------------------------ </br>
-	<b> Anime Information - Room 818 </b></br>
-	</br>
-	The Anime club will be having Anime shows on. </br></br>
+	
 	
 </p>
 
